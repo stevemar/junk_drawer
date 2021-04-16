ba30ea45fa0c1a5d773526f77425984f48f3991f
Steve Martinelli <4118756+stevemar@users.noreply.github.com>
2021-04-16 13:06:51 -0400

    Update vols.sh

--- vols.sh
+++ vols.sh
@@ -3,17 +3,19 @@ ibmcloud ks clusters | grep normal | awk '{ print $ 2}' > cluster.lst
 for block_id in $(ibmcloud sl block volume-list --column id --column notes --output JSON | jq '.[].id'); 
 do
   cluster=$(ibmcloud sl block volume-list --column id --column notes --output JSON | jq -r ".[]|select(.id==$block_id)|.notes" | jq -r .cluster); 
+  block_name=$(ibmcloud sl block volume-detail $block_id --output JSON | jq -r .username);
   grep $cluster cluster.lst > /dev/null; 
   if [ $? -eq 1 ]; then 
-    echo "cluster $cluster not found for $block_id"; 
+    echo "cluster $cluster not found for block ID $block_id / block name $block_name"; 
   fi; 
 done
 
 for file_id in $(ibmcloud sl file volume-list --column id --column notes --output JSON | jq '.[].id');
 do 
   cluster=$(ibmcloud sl file volume-list --column id --column notes --output JSON | jq -r ".[]|select(.id==$file_id)|.notes" | jq -r .cluster); 
+  file_name=$(ibmcloud sl file volume-detail $file_id --output JSON | jq -r .username);
   grep $cluster cluster.lst > /dev/null; 
   if [ $? -eq 1 ]; then 
-    echo "cluster $cluster not found for $file_id"; 
+    echo "cluster $cluster not found for file ID $file_id / file name $file_name"; 
   fi; 
 done
